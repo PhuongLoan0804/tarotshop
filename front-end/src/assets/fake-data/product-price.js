b11dd650c4a289f2c88f8a3c905e038caa6eab9e
longbuibao <blong1102@gmail.com>
2021-12-14 16:58:54 +0700

    migrate

---
+++ front-end/src/assets/fake-data/product-price.js
@@ -0,0 +1,24 @@
+const prices = [
+    {
+        display: "Dưới 500.000",
+        price: "500000"
+    },
+    {
+        display: "500.000 - 1.000.000",
+        price: "1000000"
+    },
+    {
+        display: "1.000.000 - 1.500.000",
+        price: "1500000"
+    },
+    {
+        display: "1.500.000 - 2.000.000",
+        price: "2000000"
+    },
+    {
+        display: "Trên  2.000.000",
+        price: "2500000"
+    }
+]
+
+export default prices
