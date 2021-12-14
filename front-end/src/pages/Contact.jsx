03f7b3836d88531c419c5c5e6c279162e42a53ec
PhuongLoan0804 <18521001@gm.uit.edu.vn>
2021-12-14 22:13:22 +0700

    contact page

---
+++ front-end/src/pages/Contact.jsx
@@ -0,0 +1,134 @@
+import { useState } from 'react'
+import emailjs from 'emailjs-com'
+
+import Helmet from '../components/Helmet'
+import Grid from '../components/Grid'
+import Button from '../components/Button'
+
+const Contact = (props) => {
+    const initialState = {
+        name: '',
+        email: '',
+        message: '',
+    }
+
+    const [{ name, email, message }, setState] = useState(initialState)
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setState((prevState) => ({ ...prevState, [name]: value }))
+    }
+
+    const clearState = () => setState({...initialState})
+
+    const [status, setStatus] = useState('') 
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(name, email, message)
+        emailjs.sendForm(
+            'service_pyin8zo', 'template_3xema5l', e.target, 'user_PDVUm0P2xtCSGvc6gTsKc'
+        )
+        .then(
+            (result) => {
+            console.log(result.text)
+            clearState()
+            setStatus('SUCCESS')
+            },
+            (error) => {
+            console.log(error.text)
+            }
+        )
+    }
+
+    return (
+        <Helmet title="Liên hệ">
+            <div className="contact">
+                <div className="contact__section-title">
+                    <h2>liên hệ với chúng tôi</h2>
+                    <p>Vui lòng điền vào biểu mẫu bên dưới để gửi email cho chúng tôi và chúng tôi sẽ liên hệ lại với bạn trong thời gian sớm nhất.</p>
+                    {status && renderAlert()}
+                </div>
+                {/* FORM CONTACT */}
+                <form name='sentMessage' onSubmit={handleSubmit}>
+                    <Grid col={2} mdCol={2} smCol={1} gap={20}>
+                        <div className="contact__send-message">
+                            <Grid col={2} mdCol={2} smCol={1} gap={20}>                        
+                                <div className='form-group'>
+                                    <input
+                                        type='text'
+                                        id='name'
+                                        name='name'
+                                        className='form-control'
+                                        placeholder='Họ và tên'
+                                        required
+                                        onChange={handleChange}
+                                    />
+                                    <p className='help-block text-danger'></p>
+                                </div>
+                                <div className='form-group'>
+                                <input
+                                    type='email'
+                                    id='email'
+                                    name='email'
+                                    className='form-control'
+                                    placeholder='Email'
+                                    required
+                                    onChange={handleChange}
+                                />
+                                <p className='help-block text-danger'></p>
+                                </div>
+                            </Grid>
+                            <div className='form-group'>
+                                <textarea style={{border:"solid 2px #483434"}}
+                                    name='message'
+                                    id='message'
+                                    className='form-control'
+                                    rows='5'
+                                    placeholder='Nội dung lời nhắn...'
+                                    required
+                                    onChange={handleChange}
+                                ></textarea>
+                                <p className='help-block text-danger'></p>
+                            </div>
+                            <div id='success'></div>
+                            <Button size="block">
+                                gửi
+                            </Button>
+                        </div>
+                        {/* INFORMATION */}
+                        <div className="contact__info">
+                            <div className='contact__info-item'>
+                                <h3>Thông tin liên hệ</h3>
+                                <p>
+                                    <i className='bx bx-map'></i> Địa chỉ: 
+                                    Khu phố 6, Linh Trung, Thủ Đức
+                                </p>
+                            </div>
+                            <div className='contact__info-item'>
+                                <p>
+                                    <i className='bx bx-phone' ></i> Số điện thoại: 
+                                    0123456789
+                                </p>
+                            </div>
+                            <div className='contact__info-item'>
+                                <p>
+                                    <i className='bx bx-envelope' ></i> Email: 
+                                    lonton@gmail.com
+                                </p>
+                            </div>
+                        </div>
+                    </Grid>
+                </form>
+            </div>
+        </Helmet>
+    )
+}
+
+const renderAlert = () => (
+    <div className='alert'>
+        <p>Lời nhắn của bạn đã được gửi đi thành công!!!</p>
+    </div>
+)
+
+export default Contact
