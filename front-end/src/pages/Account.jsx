e184114c83c33b3311a2d53fd52ccf5ab64eb914
PhuongLoan0804 <18521001@gm.uit.edu.vn>
2021-12-17 15:48:15 +0700

    account

--- front-end/src/pages/Account.jsx
+++ front-end/src/pages/Account.jsx
@@ -3,13 +3,22 @@ import Grid from "../components/Grid"
 import Button from "../components/Button"
 import { useState } from "react"
 
+import Modal from "../components/Modal"
+
 const Account = (props) => {
-    const initialState = {
+    const [account, setAccount] = useState({
         name: "Bùi Long",
         email: "builong1101@gmail.com",
         password: "longlong123",
-      }
-    const [{name, email, password}, setAccount] = useState(initialState)
+    })
+
+    const [cart, setCart] = useState({
+        date: "17/12/2021 9:06AM",
+        product: "Đá phong thủy",
+        quantily: "2",
+    })
+
+    const [modalOpen, setModalOpen] = useState(false);
 
     return (
         <Helmet title="Tài khoản">
@@ -21,29 +30,51 @@ const Account = (props) => {
                         </div>
                         <div className="account__info-form">
                             <div className="form-group">
-                                <label>Tên tài khoản</label>
-								<p>{props.name}</p>
+                                <label>Tên tài khoản:</label>
+								<p>{account.name}</p>
                             </div>
                             <div className="form-group">
-                                <label>Email</label>
-								<p>{props.email}</p>
+                                <label>Email:</label>
+								<p>{account.email}</p>
                             </div>
                             <div className="form-group">
-                                <label>Mật khẩu</label>
-								<p>{props.password}</p>
+                                <label>Mật khẩu:</label>
+								<p>{account.password}</p>
                             </div>
                             <div className="btn-group">
-                                <Button size="sm">Sửa</Button>
+                                <Button size="sm" className="openModalBtn" onClick={() => setModalOpen(true)}>Sửa</Button>
+                                
                             </div>
+                            
                         </div>
                     </div>
                     <div className="account__cart">
                         <div className="account__cart-title">
                             <h1>lịch sử đơn hàng</h1>
                         </div>
+                        <div className="account__cart-body">
+                            <table>
+                                <thead>
+                                    <tr>
+                                        <th>Ngày đặt hàng</th>
+                                        <th>Tên sản phẩm</th>
+                                        <th>Số lượng</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>{cart.date}</td>
+                                        <td>{cart.product}</td>
+                                        <td>{cart.quantily}</td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </Grid>
+                {modalOpen && <Modal closeModal={setModalOpen} />}
             </div>
+            
         </Helmet>
     )
 }
