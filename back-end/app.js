c203a61daff0d18e6eb5e62c5168c9423f53dfcc
longbuibao <blong1102@gmail.com>
2021-12-20 16:18:07 +0700

    fix cors

--- back-end/app.js
+++ back-end/app.js
@@ -25,12 +25,7 @@ const corsOptions = {
 
 app.use(cors(corsOptions))
 app.use(cookieParser())
-app.use(
-  cors({
-    credentials: true,
-    origin: "*",
-  })
-)
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
